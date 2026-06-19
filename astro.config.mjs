56dbc3465c09d27992ece2b91c122ed1151e2804
talalemran <talal_emran@yahoo.com>
2026-06-18 21:40:48 -0400

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+git remote add origin https://github.com/talalemran/masdarx.gitimport { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
