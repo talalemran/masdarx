a1dbd693157b06f99041bb533222767993441274
talalemran <talal_emran@yahoo.com>
2026-06-25 21:05:43 -0400

    rss fix

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,35 @@
+const postModules = import.meta.glob("./posts/*.astro", { eager: true });
+const posts = Object.values(postModules)
+  .map((mod: any) => mod.post)
+  .sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+
+const siteUrl = "https://www.masdarx.com";
+const toISODate = (d: Date) => d.toISOString().split("T")[0];
+
+export function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>مصدر إكس</title>
+    <link>${siteUrl}</link>
+    <description>أخبار البرمجيات مفتوحة المصدر والتقنيات الحديثة</description>
+    <language>ar</language>
+    <lastBuildDate>${toISODate(new Date())}</lastBuildDate>
+    <atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml" />
+${posts.map((post) => `    <item>
+      <title>${post.title}</title>
+      <link>${siteUrl}/posts/${post.slug}/</link>
+      <guid isPermaLink="true">${siteUrl}/posts/${post.slug}/</guid>
+      <description>${post.description}</description>
+      <category>${post.category}</category>
+      <pubDate>${post.pubDate ? toISODate(new Date(post.pubDate)) : ""}</pubDate>
+    </item>`).join("\n")}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/rss+xml; charset=utf-8",
+    },
+  });
+}
