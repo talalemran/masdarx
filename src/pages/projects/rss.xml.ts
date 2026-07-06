2206dccecf3e1b9ed947ddd4e2227c536138e843
talalemran <talal_emran@yahoo.com>
2026-07-05 22:59:31 -0400

    rss add

---
+++ src/pages/projects/rss.xml.ts
@@ -0,0 +1,36 @@
+import { projects } from "../../data/projects";
+
+const siteUrl = "https://www.masdarx.com";
+
+export function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
+  <channel>
+    <title>مشاريع مفتوحة المصدر - مصدر إكس</title>
+    <link>${siteUrl}/projects/</link>
+    <description>مشاريع مفتوحة المصدر على مصدر إكس</description>
+    <language>ar</language>
+    <lastBuildDate>${new Date().toISOString().split("T")[0]}</lastBuildDate>
+    <atom:link href="${siteUrl}/projects/rss.xml" rel="self" type="application/rss+xml" />
+${projects.map((project) => {
+  const cats = Array.isArray(project.category) ? project.category : [project.category];
+  const categoryXml = cats.map((c) => `      <category>${c}</category>`).join("\n");
+  return `    <item>
+      <title>${project.name}</title>
+      <link>${siteUrl}/projects/${project.slug}/</link>
+      <guid isPermaLink="true">${siteUrl}/projects/${project.slug}/</guid>
+      <description>${project.description}</description>
+${categoryXml}
+      <pubDate>${new Date().toISOString().split("T")[0]}</pubDate>
+      ${project.thumbnail ? `<media:content url="${siteUrl}${project.thumbnail}" medium="image" />` : ""}
+    </item>`;
+}).join("\n")}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/rss+xml; charset=utf-8",
+    },
+  });
+}
