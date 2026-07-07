cc5b7908c1e8841274534e965a5ed45bc177a5e8
talalemran <talal_emran@yahoo.com>
2026-07-07 13:17:28 -0400

    rss fix

--- src/pages/projects/rss.xml.ts
+++ src/pages/projects/rss.xml.ts
@@ -2,6 +2,15 @@ import { projects } from "../../data/projects";
 
 const siteUrl = "https://www.masdarx.com";
 
+function escapeXml(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
 export function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
@@ -14,12 +23,12 @@ export function GET() {
     <atom:link href="${siteUrl}/projects/rss.xml" rel="self" type="application/rss+xml" />
 ${projects.map((project) => {
   const cats = Array.isArray(project.category) ? project.category : [project.category];
-  const categoryXml = cats.map((c) => `      <category>${c}</category>`).join("\n");
+  const categoryXml = cats.map((c) => `      <category>${escapeXml(c)}</category>`).join("\n");
   return `    <item>
-      <title>${project.name}</title>
+      <title>${escapeXml(project.name)}</title>
       <link>${siteUrl}/projects/${project.slug}/</link>
       <guid isPermaLink="true">${siteUrl}/projects/${project.slug}/</guid>
-      <description>${project.description}</description>
+      <description>${escapeXml(project.description)}</description>
 ${categoryXml}
       <pubDate>${new Date().toISOString().split("T")[0]}</pubDate>
       ${project.thumbnail ? `<media:content url="${siteUrl}${project.thumbnail}" medium="image" />` : ""}
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,6 +6,15 @@ const posts = Object.values(postModules)
 const siteUrl = "https://www.masdarx.com";
 const toISODate = (d: Date) => d.toISOString().split("T")[0];
 
+function escapeXml(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
 export function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
@@ -18,12 +27,12 @@ export function GET() {
     <atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml" />
 ${posts.map((post) => {
   const cats = Array.isArray(post.category) ? post.category : [post.category];
-  const categoryXml = cats.map((c) => `      <category>${c}</category>`).join("\n");
+  const categoryXml = cats.map((c) => `      <category>${escapeXml(c)}</category>`).join("\n");
   return `    <item>
-      <title>${post.title}</title>
+      <title>${escapeXml(post.title)}</title>
       <link>${siteUrl}/posts/${post.slug}/</link>
       <guid isPermaLink="true">${siteUrl}/posts/${post.slug}/</guid>
-      <description>${post.description}</description>
+      <description>${escapeXml(post.description)}</description>
 ${categoryXml}
       <pubDate>${post.pubDate ? toISODate(new Date(post.pubDate)) : ""}</pubDate>
       ${post.thumbnail ? `<media:content url="${siteUrl}${post.thumbnail}" medium="image" />` : ""}
